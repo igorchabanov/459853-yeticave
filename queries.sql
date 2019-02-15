ef1cc127dc717746c9e22c304bedbfbd1f7a5e45
Igor Ch <igor.ch@viomedia.ru>
2019-02-15 23:07:52 +0300

    начал

--- queries.sql
+++ queries.sql
@@ -14,12 +14,12 @@ INSERT INTO user(name, email, passwd) VALUES('Иван', 'ivan@test.mail', 'test
 INSERT INTO user(name, email, passwd) VALUES('John', 'john@test.mail', 'test');
 
 -- Lots
-INSERT INTO lot(title, cat_id, start_price, img_path, author_id, user_id) VALUES('2014 Rossignol District Snowboard', 1, 10999, 'img/lot-1.jpg', 1, 2);
-INSERT INTO lot(title, cat_id, start_price, img_path, author_id, user_id) VALUES('DC Ply Mens 2016/2017 Snowboard', 1, 159999, 'img/lot-2.jpg', 2, 3);
-INSERT INTO lot(title, cat_id, start_price, img_path, author_id, user_id) VALUES('Крепления Union Contact Pro 2015 года размер L/XL', 2, 8000, 'img/lot-3.jpg', 3, 1);
-INSERT INTO lot(title, cat_id, start_price, img_path, author_id, user_id) VALUES('Ботинки для сноуборда DC Mutiny Charocal', 3, 10999, 'img/lot-4.jpg', 1, 2);
-INSERT INTO lot(title, cat_id, start_price, img_path, author_id, user_id) VALUES('Куртка для сноуборда DC Mutiny Charocal', 4, 7500, 'img/lot-5.jpg', 2, 3);
-INSERT INTO lot(title, cat_id, start_price, img_path, author_id, user_id) VALUES('Маска Oakley Canopy', 6, 5400, 'img/lot-6.jpg', 3, 1);
+INSERT INTO lot(title, cat_id, start_price, img_path, author_id, winner_id, end_date) VALUES('2014 Rossignol District Snowboard', 1, 10999, 'img/lot-1.jpg', 1, 2, '2019-02-17 20:28:36');
+INSERT INTO lot(title, cat_id, start_price, img_path, author_id, winner_id, end_date) VALUES('DC Ply Mens 2016/2017 Snowboard', 1, 159999, 'img/lot-2.jpg', 2, 3, '2019-02-16 10:28:36');
+INSERT INTO lot(title, cat_id, start_price, img_path, author_id, winner_id, end_date) VALUES('Крепления Union Contact Pro 2015 года размер L/XL', 2, 8000, 'img/lot-3.jpg', 3, 1, '2019-02-18 08:28:36');
+INSERT INTO lot(title, cat_id, start_price, img_path, author_id, winner_id, end_date) VALUES('Ботинки для сноуборда DC Mutiny Charocal', 3, 10999, 'img/lot-4.jpg', 1, 2, '2019-02-18 22:28:36');
+INSERT INTO lot(title, cat_id, start_price, img_path, author_id, winner_id, end_date) VALUES('Куртка для сноуборда DC Mutiny Charocal', 4, 7500, 'img/lot-5.jpg', 2, 3, '2019-02-21 22:28:36');
+INSERT INTO lot(title, cat_id, start_price, img_path, author_id, winner_id, end_date) VALUES('Маска Oakley Canopy', 6, 5400, 'img/lot-6.jpg', 3, 1, '2019-02-25 22:28:36');
 
 -- Rates
 INSERT INTO rate(amount, user_id, lot_id) VALUES(100, 1, 2);
--- schema.sql
+++ schema.sql
@@ -8,44 +8,45 @@ USE yeticave;
 
 CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title CHAR(128) NOT NULL
+  title CHAR(128) NOT NULL,
+  FULLTEXT (title)
 );
 
 CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
   cat_id INT NOT NULL,
   author_id INT NOT NULL,
-  user_id INT NOT NULL,
+  winner_id INT NOT NULL,
   title CHAR(128) NOT NULL,
   description TEXT,
   img_path CHAR(128),
   start_price INT NOT NULL,
   rate_step INT,
-  end_date DATE,
-  created TIMESTAMP
+  end_date DATETIME,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TABLE rate (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date TIMESTAMP,
-  amount INT,
-  user_id INT,
-  lot_id INT
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  amount INT NOT NULL,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL
 );
 
 CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  created TIMESTAMP,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email CHAR(128) NOT NULL UNIQUE,
   name CHAR(50) NOT NULL,
-  passwd CHAR(32) NOT NULL,
+  passwd CHAR(255) NOT NULL,
   img CHAR(128),
-  contact CHAR(10),
-  lot_id INT,
-  rate_id INT
+  contact CHAR(10)
 );
 
-CREATE INDEX category ON category(title);
+CREATE INDEX opened_lots ON lot(id, end_date);
+CREATE INDEX latest_rate ON rate(id, amount, created);
 
-CREATE INDEX lot ON lot(title);
+CREATE FULLTEXT INDEX title_descr ON lot(title, description);
