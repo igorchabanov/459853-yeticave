ecdfd166ba4e4501553f2c394587074a9df9f4b7
Igor Ch <igor.ch@viomedia.ru>
2019-02-18 22:40:43 +0300

    Изменил тип JOIN, вывод цены с проверкой

--- queries.sql
+++ queries.sql
@@ -14,11 +14,11 @@ INSERT INTO user(name, email, passwd) VALUES('Иван', 'ivan@test.mail', 'test
 INSERT INTO user(name, email, passwd) VALUES('John', 'john@test.mail', 'test');
 
 -- Lots
-INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('2014 Rossignol District Snowboard', 1, 10999, 'img/lot-1.jpg', 100, 1, 2, '2019-02-17');
-INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('DC Ply Mens 2016/2017 Snowboard', 1, 159999, 'img/lot-2.jpg', 100,  2, 3, '2019-02-16');
-INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('Крепления Union Contact Pro 2015 года размер L/XL', 2, 8000, 'img/lot-3.jpg', 100,  3, 1, '2019-02-18');
+INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('2014 Rossignol District Snowboard', 1, 10999, 'img/lot-1.jpg', 100, 1, 2, '2019-02-19');
+INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('DC Ply Mens 2016/2017 Snowboard', 1, 159999, 'img/lot-2.jpg', 100,  2, 3, '2019-02-20');
+INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('Крепления Union Contact Pro 2015 года размер L/XL', 2, 8000, 'img/lot-3.jpg', 100,  3, 1, '2019-02-22');
 INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('Ботинки для сноуборда DC Mutiny Charocal', 3, 10999, 'img/lot-4.jpg', 100,  1, 2, '2019-02-18');
-INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('Куртка для сноуборда DC Mutiny Charocal', 4, 7500, 'img/lot-5.jpg', 100,  2, 3, '2019-02-21');
+INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('Куртка для сноуборда DC Mutiny Charocal', 4, 7500, 'img/lot-5.jpg', 100,  2, 3, '2019-02-17');
 INSERT INTO lot(title, cat_id, start_price, img_path, rate_step, author_id, winner_id, end_date) VALUES('Маска Oakley Canopy', 6, 5400, 'img/lot-6.jpg', 100, 3, 1, '2019-02-25');
 
 -- Rates
@@ -33,10 +33,10 @@ INSERT INTO rate(amount, user_id, lot_id) VALUES(8500, 1, 3);
 SELECT * FROM category;
 
 -- Get new opened lots
-SELECT l.id, l.title, l.start_price, l.img_path, MAX(r.amount), c.title
+SELECT l.id, l.title, l.start_price, l.img_path, COALESCE(MAX(r.amount), l.start_price) AS price , c.title
 FROM lot l
 JOIN category c ON l.cat_id = c.id
-JOIN rate r ON r.lot_id = l.id
+LEFT JOIN rate r ON r.lot_id = l.id
 WHERE l.end_date > NOW()
 GROUP BY l.id
 ORDER BY l.created DESC;
